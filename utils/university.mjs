53604072f6895d6987a226369bf7bc3085e8420d
sondos-zaro <sondos.zaro@gmail.com>
2023-10-16 15:26:46 +0300

    Add utils file and university List

---
+++ utils/university.mjs
@@ -0,0 +1,21 @@
+export let universityList = [
+  {
+    collegeId: 1,
+    collegeName: "IT college",
+    Major: [
+      {
+        majorId: 1,
+        majorName: "IT",
+        course: [
+          {
+            courseId: 1,
+            courseName: "data structue",
+            numberOfHours: 3,
+            type: "college requirments",
+            prerequisites: "oob",
+          },
+        ],
+      },
+    ],
+  },
+];
