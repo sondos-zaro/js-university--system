83a4b36a47730554bbe182d9487d2b81c0feea8b
sondos-zaro <sondos.zaro@gmail.com>
2023-10-17 08:32:32 +0300

    Add updateCollege Method

--- college.mjs
+++ college.mjs
@@ -20,4 +20,13 @@ class CollegeServices {
     addCollege(college) {
         universityList.push(college);
     }
+
+    updateCollege(collegeName, newCollege) {
+        let collegeIndex = universityList.findIndex(college => collegeName === college.collegeName); 
+
+        if(collegeIndex === -1) {
+            universityList[collegeIndex] = newCollege;
+        }
+    }
 }
+
