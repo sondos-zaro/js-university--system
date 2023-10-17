59c3e4d23d5d6e81ff770040cda7da78a7535ede
sondos-zaro <sondos.zaro@gmail.com>
2023-10-17 09:07:30 +0300

    Edit conventtions

--- college.mjs
+++ college.mjs
@@ -1,30 +1,32 @@
 import { universityList } from "./utils/university.mjs";
 
 export class College {
-  constructor(collegeName) {
-    this.collegeId = this.generateNewId();
-    this.collegeName = collegeName;
+
+    constructor(collegeName) {
+        this.collegeId = this.generateNewId();
+        this.collegeName = collegeName;
   }
 
-  generateNewId() {
-    if (!universityList.length === 0) {
-      return 0;
-    }
-    let ids = universityList.map((college) => college.collegeId);
-    return Math.max(...ids) + 1;
-  }
+    generateNewId() {
+        if (universityList.length === 0) {
+            return universityList.length;
+        } else {
+            let ids = universityList.map((college) => college.collegeId);
+
+            return Math.max(...ids) + 1;
+        }
+    } 
 }
 
 class CollegeServices {
-
     addCollege(college) {
         universityList.push(college);
     }
 
     updateCollege(collegeName, newCollege) {
-        let collegeIndex = universityList.findIndex(college => collegeName === college.collegeName); 
+        let collegeIndex = universityList.findIndex(college => collegeName === college.collegeName);
 
-        if(collegeIndex === -1) {
+        if (collegeIndex === -1) {
             universityList[collegeIndex] = newCollege;
         }
     }
@@ -34,8 +36,6 @@ class CollegeServices {
             return;
         }
 
-        let isExist = universityList.map(college => college.collegeName.toLowerCase()).includes(collegeName.toLowerCase())
-        
-        return isExist;
-    }
+        return universityList.map(college => college.collegeName.toLowerCase()).includes(collegeName.toLowerCase());
+  }
 }
