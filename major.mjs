695dbd09c03f81bdcd65c0d4650baa5ce5d27f80
Moawiyah <moawiyah.nat@gmail.com>
2023-10-17 08:24:22 +0300

    Implement genrate id method

--- major.mjs
+++ major.mjs
@@ -4,7 +4,24 @@ export class Major extends College {
 
     constructor(collegeName, majorName) {
         super(collegeName);
+        this.id = this.generateNewId()
         this.majorName = majorName;
+    }
+
+    generateNewId() {
+        const ids = [];
+
+        if (!universityList.length === 0) {
+            return 0;
+        }
+
+        universityList.forEach(element => {
+            element.Major.forEach(element => {
+                ids.push(element.majorId);
+            });
+        });
+
+        return Math.max(...ids) + 1;
     }
 
 }
@@ -12,8 +29,8 @@ export class Major extends College {
 class MajorService {
 
     // Implement add major method
-    addMajor(majorName) {
-        this.majors.push(majorName)
+    addMajor() {
+
     }
 
 }
